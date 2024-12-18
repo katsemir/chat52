11cc0c3d1c57e35b54ffce51e283891a8d677578
katsemir <m.katdemir@istu.edu.ua>
2024-12-18 22:06:00 +0200

    Created ChatServer class. created ChatClient class. Allowed keyboard

--- ws/client.js
+++ ws/client.js
@@ -1,44 +1,21 @@
-const readline = require('node:readline');
+const {WebSocket} = require('ws');
 
-const { ChatClient } = require('./client/ChatClient');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-
-
-
-const sessionIdIndex = process.argv.indexOf('--sessionId');
-const nameIndex = process.argv.indexOf('--name');
-
-if (sessionIdIndex === -1 && nameIndex === -1) {
-    console.error('Arguments sessionId or name are required');
-    process.exit(1);
-}
-
-const sessionId = sessionIdIndex !== -1 ? process.argv[sessionIdIndex + 1] : null;
-const name = nameIndex !== -1 ? process.argv[nameIndex + 1] : null;
-
-init(name, sessionId);
-
-
-function init(name, sessionId) {
-    const client = new ChatClient({ url: 'ws://localhost:8080', username: name, sessionId });
-
-    client.init();
-
-    const chatInput = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
+const wsClientFactory = (id) => {
+    const ws = new WebSocket('ws://localhost:8080');
+    
+    ws.on('error', console.error);
+    
+    ws.on('open', function open() {
+        console.log(`connected ${id}`);
+        
+        ws.send(`${id} HELLO`);
     });
-
-    chatInput.on('line', (input) => {
-        if (input.trim().toLowerCase() === 'exit') {
-            chatInput.close();
-        } else {
-            client.send(input);
-        }
+    
+    ws.on('message', function message(data) {
+      console.log(`${id} received: %s`, data);
     });
 }
+
+const wsClient1 = wsClientFactory(1);
+const wsClient2 = wsClientFactory(2);
+const wsClient3 = wsClientFactory(3);
